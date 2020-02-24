7ae4592abea1c8b801ff0c7e24227a71a39464c5
Karlo Lopez (iMac) <karlo@qzbyte.com>
2020-02-23 22:10:28 -0600

    Final Delibery

--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -6,7 +6,18 @@ namespace HolaXamarin.iOS
 {
     public partial class ViewController : UIViewController
     {
-        
+
+        int value1 = 0;
+        int value2 = 0;
+
+        /*
+         * Índice para saber la operación a realizar donde:
+         * 0 suma
+         * 1 resta
+         * 2 multiplica
+         */
+
+        int operation = 0;
 
         public ViewController(IntPtr handle) : base(handle)
         {
@@ -24,9 +35,149 @@ namespace HolaXamarin.iOS
             // Release any cached data, images, etc that aren't in use.		
         }
 
-        partial void UIButton817_TouchUpInside(UIButton sender)
+        /*
+         * addToStringNumber
+         * 
+         * Parámetros de entrada:
+         * entero i - representa el número que se agregará a la cadena a desplegar en la calculadora
+         *
+         *
+         * La intención de este método es agregar a la DERECHA de los números que actualmente se
+         * están mostrando en la calculadora el nuevo número {i} introducido.
+         *
+         * ¿Por qué a la derecha?
+         * Por que es el flujo de armado de decimales común para introducir datos en una calcularora
+         *
+         * Ejemplo:
+         *
+         * Si quiero introducir el número 13 y acutlamente la cadena es 0 CERO, lo que debo hacer sería lo siguiente:
+         *
+         * El usuario presiona 1:
+         * - Eliminar el CERO y agregar el 1
+         *
+         * El usuario presiona 3:
+         * - Agregar el 3 a la derecha del 1
+         *
+         * 1<-- Aquí va el 3
+         * 13
+         * 
+         */
+
+        public void addToStringNumber(int i) {
+            if (String.Equals(resultLabel.Text, "0")) {
+                resultLabel.Text = "";
+            }
+            resultLabel.Text = resultLabel.Text + i;
+        }
+
+        /*
+         * Botónes númericos, TOUCH UP INSIDES
+         *
+         * Estos eventos se llaman cuando el usuario toca sobre uno de los botónes
+         * con valor númerico. CAda uno llama a addToStringNumber con el valor correspondiente.
+         */
+
+        partial void Uno_TouchUpInside(UIButton sender)
         {
-            throw new NotImplementedException();
+            addToStringNumber(1);
+
+            //Este throw parece una línea que se generá por defecto al agregar un método "Touch Inside" desde el constructor de UI
+            //throw new NotImplementedException();
+        }
+
+        partial void Dos_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(2);
+        }
+
+        partial void Tres_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(3);
+        }
+
+        partial void Cuatro_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(4);
+        }
+
+        partial void Cinco_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(5);
+        }
+
+        partial void Seis_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(6);
+        }
+
+        partial void Siete_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(7);
+        }
+
+        partial void Ocho_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(8);
+        }
+
+        partial void Nueve_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(9);
+        }
+
+        partial void Cero_TouchUpInside(UIButton sender)
+        {
+            addToStringNumber(0);
+        }
+
+        /*
+         * Eventos de botones de suma.
+         */
+
+        partial void Sumar_TouchUpInside(UIButton sender)
+        {
+            operation = 0;
+            value1 = Int32.Parse(resultLabel.Text);
+            resultLabel.Text = "0";
+        }
+
+        partial void Restar_TouchUpInside(UIButton sender)
+        {
+            operation = 1;
+            value1 = Int32.Parse(resultLabel.Text);
+            resultLabel.Text = "0";
+        }
+
+        partial void Multiplicar_TouchUpInside(UIButton sender)
+        {
+            operation = 2;
+            value1 = Int32.Parse(resultLabel.Text);
+            resultLabel.Text = "0";
+        }
+
+        partial void EqualsButton_TouchUpInside(UIButton sender)
+        {
+            value2 = Int32.Parse(resultLabel.Text);
+
+            if (operation == 0)
+            {
+                resultLabel.Text = "" + (value1 + value2);
+            }
+            else if (operation == 1)
+            {
+                resultLabel.Text = "" + (value1 - value2);
+            }
+            else {
+                resultLabel.Text = "" + (value1 * value2);
+            }
+        }
+
+        partial void Acbutton_TouchUpInside(UIButton sender)
+        {
+            value1 = 0;
+            value2 = 0;
+            resultLabel.Text = "0";
+            operation = 0;
         }
     }
 }
--- iOS/ViewController.designer.cs
+++ iOS/ViewController.designer.cs
@@ -16,12 +16,211 @@ namespace HolaXamarin.iOS
         [Outlet]
         UIKit.UIButton Button { get; set; }
 
-        [Action ("UIButton817_TouchUpInside:")]
+        [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        partial void UIButton817_TouchUpInside (UIKit.UIButton sender);
+        UIKit.UIButton acbutton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton cero { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton cinco { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton cuatro { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton dos { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton equalsButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton multiplicar { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton nueve { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ocho { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton restar { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel resultLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton seis { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton siete { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton sumar { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton tres { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton uno { get; set; }
+
+        [Action ("Acbutton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Acbutton_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Cero_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Cero_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Cinco_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Cinco_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Cuatro_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Cuatro_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Dos_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Dos_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("EqualsButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void EqualsButton_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Multiplicar_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Multiplicar_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Nueve_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Nueve_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Ocho_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Ocho_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Restar_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Restar_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Seis_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Seis_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Siete_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Siete_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Sumar_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Sumar_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Tres_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Tres_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("Uno_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Uno_TouchUpInside (UIKit.UIButton sender);
 
         void ReleaseDesignerOutlets ()
         {
+            if (acbutton != null) {
+                acbutton.Dispose ();
+                acbutton = null;
+            }
+
+            if (cero != null) {
+                cero.Dispose ();
+                cero = null;
+            }
+
+            if (cinco != null) {
+                cinco.Dispose ();
+                cinco = null;
+            }
+
+            if (cuatro != null) {
+                cuatro.Dispose ();
+                cuatro = null;
+            }
+
+            if (dos != null) {
+                dos.Dispose ();
+                dos = null;
+            }
+
+            if (equalsButton != null) {
+                equalsButton.Dispose ();
+                equalsButton = null;
+            }
+
+            if (multiplicar != null) {
+                multiplicar.Dispose ();
+                multiplicar = null;
+            }
+
+            if (nueve != null) {
+                nueve.Dispose ();
+                nueve = null;
+            }
+
+            if (ocho != null) {
+                ocho.Dispose ();
+                ocho = null;
+            }
+
+            if (restar != null) {
+                restar.Dispose ();
+                restar = null;
+            }
+
+            if (resultLabel != null) {
+                resultLabel.Dispose ();
+                resultLabel = null;
+            }
+
+            if (seis != null) {
+                seis.Dispose ();
+                seis = null;
+            }
+
+            if (siete != null) {
+                siete.Dispose ();
+                siete = null;
+            }
+
+            if (sumar != null) {
+                sumar.Dispose ();
+                sumar = null;
+            }
+
+            if (tres != null) {
+                tres.Dispose ();
+                tres = null;
+            }
+
+            if (uno != null) {
+                uno.Dispose ();
+                uno = null;
+            }
         }
     }
 }
